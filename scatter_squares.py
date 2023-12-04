42c677741e579318b6f9680cc0f85436b1d4d846
observer <ozzyundead@gmail.com>
2023-12-04 17:19:33 +0200

    add new file and study how to make dotted graph

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn-v0_8')
-# fig = plt.subplot()
 ax = plt.subplot()
-ax.plot(input_values, squares, linewidth=3)
+
+# add dot on the graph (s - size)
+ax.scatter(x_values, y_values, s=100)
 
 # Set graff name and name each of axis
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,6 +16,5 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set fontsize for signature on marking
 ax.tick_params(axis='both', labelsize=14)
-
 
 plt.show()
